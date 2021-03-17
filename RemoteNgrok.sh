d84b5135d2bfb47ead91b436273b57266974a925
F4ICR <55432858+F4ICR@users.noreply.github.com>
2021-03-18 00:01:43 +0100

    Add files via upload

--- RemoteNgrok.sh
+++ RemoteNgrok.sh
@@ -16,17 +16,17 @@ sleep 5
 
 # Creation entete mail.txt pour l'envoi de la nouvelle URL du tunnel
 echo 'From: "ngrok" <ngrok@ngrok.com>' > mail.txt
-echo 'To: "Pascal" <pascal.paquet@gmail.com>' >> mail.txt
+echo 'To: "VOTRE_PRENOM_OU_AUTRE" <VOTRE_MAIL>' >> mail.txt
 echo 'Subject: Tunnel ngrok' >> mail.txt
 echo '.' >> mail.txt
 
 # On recupere l'URL via l'api et on decoupe la partie necessaire que l'on insere au fichier mail.txt qui sera envoyer
-curl http://127.0.0.1:4040/api/tunnels |grep -o '"public_url":"http://*.*.io"'| cut -d ":" -f 2,3 >> mail.txt
+curl http://127.0.0.1:4040/api/tunnels | grep -o '"public_url":"http://*.*.io"' | cut -d ":" -f 2,3 >> mail.txt
 sleep 5
 
 # On envoi le mail via curl avec pour contenu le ficher mail.txt
 curl --url 'smtps://smtp.gmail.com:465' --ssl-reqd --mail-from 'ngrok@ngrok.com'\
- --mail-rcpt 'pascal.paquet@gmail.com' --upload-file mail.txt --user 'pascal.paquet@gmail.com:wlubhwfcogtdfyfb' --insecure
+ --mail-rcpt 'MAIL_DESTINATAIRE' --upload-file mail.txt --user 'VOTRE_MAIL_DE_CONNEXION:MOT_DE_PASSE' --insecure
 
 else
 echo "ngrok en fonction"
