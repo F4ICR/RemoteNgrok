215a37a025430159978cb2a55013b5aa31af24e6
Pascal <55432858+F4ICR@users.noreply.github.com>
2021-03-19 07:34:58 +0100

    Update RemoteNgrok.sh

--- RemoteNgrok.sh
+++ RemoteNgrok.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # F4ICR 2021
 
-# On verifie la presence du processus
+# Verification de la presence du processus
 testNGROK=$(pgrep -f "ngrok.yml 3000" |wc -l)
 
 # Si different de 1
@@ -9,7 +9,7 @@ if [ $testNGROK != 1 ]
 then
 echo "ngrok arreter"
 
-# On redemarre le tunnel ngrok
+# Demarrage du tunnel ngrok
 cd /root/
 nohup ./ngrok http -config=.ngrok2/ngrok.yml 3000 &
 sleep 5
@@ -20,11 +20,11 @@ echo 'To: "VOTRE_PRENOM_OU_AUTRE" <VOTRE_MAIL>' >> mail.txt
 echo 'Subject: Tunnel ngrok' >> mail.txt
 echo '.' >> mail.txt
 
-# On recupere l'URL via l'api et on decoupe la partie necessaire que l'on insere au fichier mail.txt qui sera envoyer
+# Recuperation de l'URL via l'api et on decoupe la partie necessaire que l'on insere au fichier mail.txt qui sera envoyer
 curl http://127.0.0.1:4040/api/tunnels | grep -o '"public_url":"http://*.*.io"' | cut -d ":" -f 2,3 >> mail.txt
 sleep 5
 
-# On envoi le mail via curl avec pour contenu le ficher mail.txt
+# Envoie le mail via curl avec pour contenu le ficher mail.txt
 curl --url 'smtps://smtp.gmail.com:465' --ssl-reqd --mail-from 'ngrok@ngrok.com'\
  --mail-rcpt 'MAIL_DESTINATAIRE' --upload-file mail.txt --user 'VOTRE_MAIL_DE_CONNEXION:MOT_DE_PASSE' --insecure
 
